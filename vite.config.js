8af9b0d06d8fa52ddf86771ae489ff253f69e794
gxnny19 <wkdrudrjs010@gmail.com>
2025-08-20 14:27:34 +0900

    0820

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/react-site/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/react-site/' : '/',
+  }
 })
+
 // export default defineConfig({
 //   base: '/react-site/',
 //   plugins: [react()],
