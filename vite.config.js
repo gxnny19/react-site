c559b320055691ab7dae9b8479e3c9df9ff4c874
gxnny19 <wkdrudrjs010@gmail.com>
2025-08-25 16:53:11 +0900

    0825

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/react-site/', 
+  plugins: [react()],
+  base: '/react-site/',
 })
